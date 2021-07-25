4a7e8ee85bb9347e4d564b5e05360643dc005daf
Jaroslav <j@bereza.cz>
2021-07-26 01:05:49 +0200

    Fixes the way how value is shown

--- src/inspector/components/JSONTree/objType.tsx
+++ src/inspector/components/JSONTree/objType.tsx
@@ -4,15 +4,21 @@ import { TNodeType } from "./types";
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export function objType(obj: any): TNodeType {
-	
-	if (obj!==null && typeof obj === "object" && typeof obj[Symbol.iterator] === "function") {
+	let type: TNodeType;
+	try {
+		type = Object.prototype.toString.call(obj).slice(8, -1) as TNodeType;
+		
+	} catch (e) {
+		return "Proxy";
+	}
+	if (type === "Object" && typeof obj[Symbol.iterator] === "function") {
 		return "Iterable";
 	}
-	if (obj!==null && typeof obj === "object" && obj.constructor !== Object && obj instanceof Object) {
+
+	if (type === "Custom" && obj.constructor !== Object && obj instanceof Object) {
 		// For projects implementing objects overriding `.prototype[Symbol.toStringTag]`
 		return "Object";
 	}
 
-	const type = Object.prototype.toString.call(obj).slice(8, -1) as TNodeType;
 	return type;
 }
