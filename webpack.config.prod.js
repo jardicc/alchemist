491ba5f5425bf6fc927ca8dcfa7e4e94f1a4e5f0
Jaroslav <j@bereza.cz>
2021-09-08 21:48:52 +0200

    makes production build more debugable

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,14 +7,23 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
 	mode: "production",
 	optimization: {
+		moduleIds:"named",
 		minimize: true,
 		minimizer: [new TerserPlugin(
 			{
 				extractComments: true,
 				parallel: true,
 				terserOptions: {
+					mangle: false,
 					compress: {
+						conditionals :false,
 						drop_console: true,
+						drop_debugger: true,
+						comparisons: false,
+						collapse_vars: false,
+						booleans: false,
+						inline: false,
+						keep_classnames: true,
 					},
 				},
 			},
