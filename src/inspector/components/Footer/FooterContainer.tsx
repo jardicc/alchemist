3e1e545411070bb05869abb6a84610b47f46e1fa
Jaroslav <j@bereza.cz>
2022-08-06 21:01:57 +0200

    Footer cleanup

--- src/inspector/components/Footer/FooterContainer.tsx
+++ src/inspector/components/Footer/FooterContainer.tsx
@@ -4,7 +4,8 @@ import React from "react";
 import "./Footer.less";
 import { GetInfo } from "../../classes/GetInfo";
 import { Main } from "../../../shared/classes/Main";
-import { Dispatch } from "redux";
+import {Dispatch} from "redux";
+import {valid,coerce} from "semver";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const versions = require("uxp").versions;
 
@@ -14,16 +15,20 @@ class Footer extends React.Component<TFooter,Record<string,unknown>> {
 	}
 
 	public render(): React.ReactNode{
-		const { parentPanel } = this.props;
 		const psVersionSegments = GetInfo.getBuildString();
+		const uxpVersion = valid(coerce(versions?.uxp?.split?.("-")?.[1])) ?? "Parser error";
+		const psVersion = valid(coerce(psVersionSegments?.split?.(" ")?.[0])) ?? "Parser error";
+		const buildParts = psVersionSegments?.split(" ")?.[1]?.split(".");
+		const buildNumber = buildParts?.length >= 3 ? (buildParts[1] + buildParts[2]) : "";
+
 		return (
 			<div className="Footer">
 				<div className="versionBar">
 					<span className="version">{versions.plugin} {Main.devMode ? "DEV":"PROD"}</span>
 					<span> / </span>
-					<span className="version">{versions.uxp}</span>
+					<span className="version">UXP: {uxpVersion}</span>
 					<span> / PS: </span>
-					<span className="version">{psVersionSegments}</span>
+					<span className="version">{psVersion} ({buildNumber})</span>
 				</div>
 				<div className="spread"></div>
 				<div className="copy">Copyright © {new Date().getFullYear()} <a href="https://bereza.cz">Bereza.cz</a></div>
